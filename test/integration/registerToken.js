c7165ab7c3e3345fdad995fa79e0b70bd8c9af5c
gianfebrian <gianfebrian@gmail.com>
2016-02-04 00:06:24 +0700

    add integration test

---
+++ test/integration/registerToken.js
@@ -0,0 +1,26 @@
+var chai = require('chai'),
+  factories = require('chai-factories'),
+  expect = chai.expect,
+  setup = require('./../_setup.js')._setup,
+  request = require('request');
+
+describe('integration', function() {
+
+  before(function(done) {
+    chai.use(factories);
+    require('./../factories/gcmToken')(chai);
+
+    setup.init(done);
+  });
+
+  it('api/registerGCMToken', function(done) {
+    request.post(setup.testUrl + '/registerGCMToken', {
+      form: chai.create('gcmToken')
+    }, function(err, response, body) {
+      body = JSON.parse(body);
+      expect(body.error).to.be.undefined;
+      done();
+    });
+  });
+
+});
